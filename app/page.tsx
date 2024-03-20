8e2dde8d8bfb5c79a121ae0ebb47773f84576804
Pablo Thasi <thasipablo@gmail.com>
2024-03-20 12:01:14 +0100

    add ui components

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <main>Hi, it&apos;s your review assistant</main>
-  );
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hi, it&apos;s your review assistant</h1>
+      <Button size="sm">Click me</Button>
+    </main>
+  );
+}
